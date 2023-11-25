2755480c0b89b11d87228f449d98068e93175515
XenoZ <xenoz0096@gmail.com>
2023-11-25 21:04:52 +0530

    v1.0-beta-release

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Cart, Hero, Sales, FlexContent, Stories, Footer, Navbar} from './components';
+import { heroapi, popularsales, toprateslaes, highlight, sneaker, story, footerAPI } from './data/data.js';
+
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Cart/>
+    <main className='flex flex-col gap-16 relative' >
+    <Hero heroapi={heroapi} />
+    <Sales endpoint={popularsales} ifExists />
+    <FlexContent endpoint={highlight} ifExists />
+    <Sales endpoint={toprateslaes} />
+    <FlexContent endpoint={sneaker} />
+    <Stories story={story} />
+    </main>
+    <Footer footerAPI={footerAPI} />
+    </>
+  )
+}
+
+export default App
